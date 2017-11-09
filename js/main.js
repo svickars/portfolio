0af858b8ee901980747f25e66f7d2da0bd14ae50
samvickars <sam.vickars@gmail.com>
2017-11-09 14:03:44 -0800

    Update main pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function($) {
 
+
+  //move nav element position according to window width
+  moveNavigation();
+  $(window).on('resize', function() {
+    (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300): window.requestAnimationFrame(moveNavigation);
+  });
+
   //mobile version - open/close navigation
   $('.cd-nav-trigger').on('click', function(event) {
     event.preventDefault();
@@ -43,3 +50,54 @@ jQuery(document).ready(function($) {
     return (mq == 'mobile') ? false : true;
   }
 });
+
+var titleHeight = $('.title').height();
+
+$(window).scroll(function() {
+  if ($(window).width() > 768) {
+    if ($(window).scrollTop() > titleHeight) {
+      $('.project-info').css('position', 'fixed').css('top', '0');
+    } else {
+      $('.project-info').css('position', 'static');
+    }
+  } else {
+    if ($(window).scrollTop() > titleHeight) {
+      $('.project-info').css('position', 'static');
+    } else {
+      $('.project-info').css('position', 'static');
+    }
+  }
+});
+
+
+
+
+$('.imzoom')
+  // imzoom mouse actions
+  .on('mouseover', function() {
+    $(this).children('.photo').css({
+      'transform': 'scale(' + $(this).attr('data-scale') + ')'
+    });
+  })
+  .on('mouseout', function() {
+    $(this).children('.photo').css({
+      'transform': 'scale(1)'
+    });
+  })
+  .on('mousemove', function(e) {
+    $(this).children('.photo').css({
+      'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 + '%'
+    });
+  })
+  // imzooms set up
+  .each(function() {
+    $(this)
+      // add a photo container
+      .append('<div class="photo"></div>')
+      // some text just to show zoom level on current item in this example
+      .append('<div class="txt">HOVER TO ZOOM</div>')
+      // set up a background image for each imzoom based on data-image attribute
+      .children('.photo').css({
+        'background-image': 'url(' + $(this).attr('data-image') + ')'
+      });
+  })
