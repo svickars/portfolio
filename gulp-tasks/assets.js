334c5244dd37d252d4cec05d35c166c4d85bd5f0
svickars <sam.vickars@gmail.com>
2020-02-18 16:24:07 -0800

    rebuild

--- gulp-tasks/assets.js
+++ gulp-tasks/assets.js
@@ -4,39 +4,39 @@ var cache = require('gulp-cache');
 
 // optimize images
 gulp.task('opt:images', function() {
-  return gulp.src('src/assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
-    .pipe(cache(imagemin({
-      interlaced: true
-    })))
-    .pipe(gulp.dest('docs/assets/img'))
+	return gulp.src('src/assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
+		// .pipe(cache(imagemin({
+		//   interlaced: true
+		// })))
+		.pipe(gulp.dest('docs/assets/img'))
 });
 
 gulp.task('assets:fonts', function() {
-  return gulp.src('src/assets/fonts/**/*')
-    .pipe(gulp.dest('docs/assets/fonts'))
+	return gulp.src('src/assets/fonts/**/*')
+		.pipe(gulp.dest('docs/assets/fonts'))
 })
 
 gulp.task('assets:scripts', function() {
-  return gulp.src('src/assets/tools/js/**/*')
-    .pipe(gulp.dest('docs/assets/tools/js'))
+	return gulp.src('src/assets/tools/js/**/*')
+		.pipe(gulp.dest('docs/assets/tools/js'))
 })
 
 gulp.task('assets:styles', function() {
-  return gulp.src('src/assets/tools/css/**/*')
-    .pipe(gulp.dest('docs/assets/tools/css'))
+	return gulp.src('src/assets/tools/css/**/*')
+		.pipe(gulp.dest('docs/assets/tools/css'))
 })
 
 gulp.task('assets:data', function() {
-  return gulp.src('src/assets/data/**/*')
-    .pipe(gulp.dest('docs/assets/data'))
+	return gulp.src('src/assets/data/**/*')
+		.pipe(gulp.dest('docs/assets/data'))
 })
 
 gulp.task('assets:cname', function() {
-  return gulp.src('CNAME')
-    .pipe(gulp.dest('docs/'))
+	return gulp.src('CNAME')
+		.pipe(gulp.dest('docs/'))
 })
 
 gulp.task('projects', function() {
-  return gulp.src('src/projects/**/*')
-    .pipe(gulp.dest('docs/'))
+	return gulp.src('src/projects/**/*')
+		.pipe(gulp.dest('docs/'))
 })
--- gulpfile.js
+++ gulpfile.js
@@ -7,38 +7,38 @@ var del = require('del');
 requireDir('./gulp-tasks');
 
 gulp.task('watch', ['browserSync', 'css:compile'], function() {
-  gulp.watch('src/css/**/*.scss', ['css:compile', browserSync.reload]);
-  gulp.watch('src/*.hbs', ['html:dev', browserSync.reload]);
-  gulp.watch('src/assets/copy/*.json', ['html:dev', browserSync.reload]);
-  // gulp.watch('src/*.html', [browserSync.reload]);
-  gulp.watch('src/js/**/*.js', ['bundle', browserSync.reload]);
+	gulp.watch('src/css/**/*.scss', ['css:compile', browserSync.reload]);
+	gulp.watch('src/*.hbs', ['html:dev', browserSync.reload]);
+	gulp.watch('src/assets/copy/*.json', ['html:dev', browserSync.reload]);
+	// gulp.watch('src/*.html', [browserSync.reload]);
+	gulp.watch('src/js/**/*.js', ['bundle', browserSync.reload]);
 })
 
 gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'src'
-    },
-    port: 4000,
-    notify: false,
-    ghostMode: false,
-    online: true,
-  })
+	browserSync.init({
+		server: {
+			baseDir: 'src'
+		},
+		port: 4000,
+		notify: false,
+		ghostMode: false,
+		online: true,
+	})
 })
 
 gulp.task('clean:docs', function() {
-  return del.sync('docs');
+	return del.sync('docs');
 });
 
 gulp.task('build', function(callback) {
-  runSequence('clean:docs', 'html:dev',
-    ['bundle', 'css:compile', 'css:purge', 'opt:images', 'assets:fonts', 'assets:styles', 'assets:scripts', 'assets:data', 'assets:cname', 'projects'],
-    callback
-  )
+	runSequence('clean:docs', 'html:dev',
+		['bundle', 'css:compile', 'css:purge', 'opt:images', 'assets:fonts', 'assets:styles', 'assets:scripts', 'assets:data', 'assets:cname', 'projects'],
+		callback
+	)
 });
 
 gulp.task('default', function(callback) {
-  runSequence('google:fonts', ['html:dev', 'css:compile', 'browserSync', 'watch'],
-    callback
-  )
+	runSequence('google:fonts', ['html:dev', 'css:compile', 'browserSync', 'watch'],
+		callback
+	)
 });
