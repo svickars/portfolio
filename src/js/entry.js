7efa25d26dc4021d66230224158f98abb1e64e93
svickars <sam.vickars@gmail.com>
2020-06-04 15:22:12 -0700

    resize

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,16 +1,39 @@
 import * as d3 from "d3";
 
+let previousWidth = 0;
+
 init();
 
 function init() {
-  d3.selectAll(".project").on("click", function () {
-    const newHeight = d3.select(this).classed("expanded")
+  d3.selectAll(".project .project-title").on("click", function () {
+    const newHeight = d3.select(this.parentNode).classed("expanded")
       ? 0
-      : d3.select(this).select(".inner").node().offsetHeight + 37;
+      : d3.select(this.parentNode).select(".inner").node().offsetHeight + 37;
 
-    d3.select(this)
-      .classed("expanded", !d3.select(this).classed("expanded"))
+    d3.select(this.parentNode)
+      .classed("expanded", !d3.select(this.parentNode).classed("expanded"))
       .select(".inner-wrapper")
       .style("height", `${newHeight}px`);
   });
+}
+
+window.addEventListener("resize", function () {
+  var currentWidth = window.innerWidth;
+
+  if (previousWidth !== currentWidth) {
+    previousWidth = currentWidth;
+
+    resizeGeneral();
+  }
+});
+
+function resizeGeneral() {
+  d3.selectAll(".project.expanded")
+    .select(".inner-wrapper")
+    .style("height", function () {
+      const newHeight =
+        d3.select(this).select(".inner").node().offsetHeight + 37;
+
+      return `${newHeight}px`;
+    });
 }
